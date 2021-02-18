7a557ee84082c9c03d68f22a5773c54e812df656
antonbadretdinov <antonbad03@gmail.com>
2021-02-18 18:03:44 +0700

    add BD

---
+++ app/src/main/java/com/example/bd/DatabaseHelper.java
@@ -0,0 +1,4 @@
+package com.example.bd;
+
+public class DatabaseHelper {
+}
---
+++ app/src/main/java/com/example/bd/information.java
@@ -0,0 +1,13 @@
+package com.example.bd;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
